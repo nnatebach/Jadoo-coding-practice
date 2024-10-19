ec39b6766fa61eeb8fbc5520e583a8ac664d6067
nnatebach <nhanmbsgvi@gmail.com>
2024-10-19 16:16:18 +0700

    [add] swiper

---
+++ project/script.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+
+function testimonials_Swiper () {
+  var swiper = new Swiper(".js-testimonials-swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+}
+
+testimonials_Swiper();
